b083bc9309502ee2262ebf074497137dff29c723
Caio Chaves  <caiojob@outlook.pt>
2023-08-27 19:49:40 +0200

    Uploading project

--- SignUpScreen/SignUpScreen/SecondScreenViewController.swift
+++ SignUpScreen/SignUpScreen/SecondScreenViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
+import Combine
 
 class SecondScreenViewController: UIViewController {
-
+    
     //MARK: Properties
     private var nameTextField: UITextField = {
         let textField = UITextField()
@@ -56,7 +57,7 @@ class SecondScreenViewController: UIViewController {
     
     private var registerButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .systemBlue
+        button.backgroundColor = UIColor.systemBlue
         button.setTitle("Register", for: .normal)
         button.setTitleColor(UIColor.white, for: .normal)
         button.layer.cornerRadius = 10
@@ -64,14 +65,30 @@ class SecondScreenViewController: UIViewController {
         return button
     }()
     
+    @Published private var name = ""
+    
+    @Published private var email = ""
+    
+    @Published private var password = ""
+    
+    @Published private var confirmPass = ""
+    
+    var createButton: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         configureLayout()
+        configuringTextField()
     }
     
     //MARK: Configuring layout
     private func configureLayout() {
+        
+        // Title
+        navigationItem.title = "Sign up"
+        navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
         
         // Setting background
         let width = UIScreen.main.bounds.size.width
@@ -97,8 +114,8 @@ class SecondScreenViewController: UIViewController {
         registerButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-        
-            nameTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+            
+            nameTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
             nameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             nameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             nameTextField.heightAnchor.constraint(equalToConstant: 50),
@@ -126,5 +143,112 @@ class SecondScreenViewController: UIViewController {
         ])
         
     }
+    
+    private func configuringTextField() {
+        
+        nameTextField.delegate = self
+        emailTextField.delegate = self
+        passTextField.delegate = self
+        confirmPassTextField.delegate = self
+        
+        registerButton.backgroundColor = registerButton.isEnabled ? UIColor.systemBlue : UIColor.systemBlue.withAlphaComponent(0.2)
+        
+        registerButton.addTarget(self, action: #selector(registerDidTapped), for: .touchUpInside)
+        
+        createButton = validatedCredentials
+            .map { $0 != nil }
+            .receive(on: RunLoop.main)
+            .assign(to: \.isEnabled, on: registerButton)
+        
+    }
+    
+    var validateName: AnyPublisher<String?, Never> {
+        return $name
+            .debounce(for: 0.5, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .flatMap { name in
+                return Future { promise in
+                    self.nameAvailable(name) { available in
+                        promise(.success(available ? name : nil))
+                    }
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    var validateEmail: AnyPublisher<String?, Never> {
+        return $email
+            .debounce(for: 0.5, scheduler: RunLoop.main)
+            .flatMap { emailText in
+                return Future { promise in
+                    self.nameAvailable(emailText) { available in
+                        promise(.success(available ? emailText : nil))
+                    }
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    var validatePassword: AnyPublisher<String?, Never> {
+        return Publishers.CombineLatest($password, $confirmPass)
+            .map { pass, passAgain in
+                guard pass == passAgain, pass.count > 0 else { return nil }
+                return pass
+            }
+            .map {
+                ($0 ?? "") == "password1" ? nil : $0
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    var validatedCredentials: AnyPublisher<(String, String, String)?, Never> {
+        return Publishers.CombineLatest3(validateName, validateEmail, validatePassword)
+            .receive(on: RunLoop.main)
+            .map { nameText, emailText, passwordText in
+                guard let name = nameText, let email = emailText, let password = passwordText else { return nil }
+                return (name, email, password)
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    private func nameAvailable(_ name: String, completion: (Bool) -> Void) {
+        completion(!name.isEmpty)
+    }
+    
+    //MARK: Button action
+    @objc private func registerDidTapped() {
+        let vc = ThirdScreenViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
 
+extension SecondScreenViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let textFieldText = textField.text ?? ""
+        let text = (textFieldText as NSString).replacingCharacters(in: range, with: string)
+        
+        if textField == nameTextField { name = text }
+        if textField == emailTextField { email = text }
+        if textField == passTextField { password = text }
+        if textField == confirmPassTextField { confirmPass = text }
+        
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == nameTextField {
+            emailTextField.becomeFirstResponder()
+        } else if textField == emailTextField {
+            passTextField.becomeFirstResponder()
+        } else if textField == passTextField {
+            confirmPassTextField.becomeFirstResponder()
+        } else if textField == confirmPassTextField {
+            textField.resignFirstResponder()
+        }
+        
+        return true
+    }
+    
 }
---
+++ SignUpScreen/SignUpScreen/ThirdScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ThirdScreenViewController.swift
+//  SignUpScreen
+//
+//  Created by Caio Chaves on 27.08.23.
+//
+
+import UIKit
+
+class ThirdScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
